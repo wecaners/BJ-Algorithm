09f600f9bb6959a26b268d4c6dcdaf9c386104e9
woody <90304025+wecaners@users.noreply.github.com>
2023-10-17 10:29:39 +0900

    [level 1] Title: 음양 더하기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,11 +1,7 @@
 function solution(absolutes, signs) {
     let answer = 0;
     for(let i = 0; i < signs.length; i++) {
-        if(signs[i] === true) {
-            answer += absolutes[i];
-        } else if(signs[i] === false) {
-            answer += absolutes[i] * -1;
-        }
+        signs[i] ? answer += absolutes[i] : answer += (absolutes[i] * -1);
     }
     return answer;
 }
