e12914b75245c794b8a26833f94f1cac9e523ce0
woody <90304025+wecaners@users.noreply.github.com>
2023-10-28 10:41:35 +0900

    [level 1] Title: 최소직사각형, Time: 6.70 ms, Memory: 41.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,17 @@
+function solution(sizes) {
+    // 제일 긴 길이 기준 하나 정함 -> width
+    // 첫번째 원소랑, 두번째 원소 비교해서 큰 값을 앞에다 놓음 
+    // 제일 긴 가로 * 제일 긴 세로 값 리턴
+    let maxWidth = 0;
+    let maxHeight = 0;
+    sizes.forEach((item) => {
+        const [width, height] = item.sort((a,b) =>  b - a);
+        if(width > maxWidth) {
+            maxWidth = width;
+        }
+        if (height > maxHeight) {
+            maxHeight = height;
+        }
+    })
+    return maxWidth * maxHeight;
+}
